c20e8d2cb4bfdf05a0b1d1d7f751f1b726aec07b
yogeshu <yogeshbhavsar1994@gmail.com>
2021-02-14 15:17:13 +0530

    added image component , fetch images and map use

--- src/containers/Photos.js
+++ src/containers/Photos.js
@@ -1,9 +1,20 @@
-import React from "react"
+import React, {useContext} from "react"
+
+
+import Image from '../components/Images';
+
+import {getClass} from '../data'
+import {Context } from '../context';
 
 function Photos() {
+    const {photos} = useContext(Context);
+    const images = photos.map((photo ,i ) =>(
+      <Image key={i} img={photo} className={getClass(i)}/>
+    ))
+
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+           {images}
         </main>
     )
 }
--- src/context.js
+++ src/context.js
@@ -5,13 +5,12 @@ const Context = React.createContext();
 function ContextProvider(props) {
   const [photos, setPhotos] = React.useState([]);
 
-  const url =
-    "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+  const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
 
   React.useEffect(() => {
     fetch(url)
       .then((res) => res.json())
-      .them((data) => setPhotos(data));
+      .then((data) => setPhotos(data));
   }, []);
 
   return (
