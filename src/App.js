871e1b78e94654851cf3c8e17df21468e4f0a57d
yogeshu <yogeshbhavsar1994@gmail.com>
2021-02-14 14:54:20 +0530

    added react router dom and router setup for home and cart page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Cart from "./containers/Cart";
+import Photos from "./containers/Photos";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     Ecommerce site 
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route path="/Cart">
+          <Cart />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router } from 'react-router-dom'
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router> <React.StrictMode>
     <App />
-  </React.StrictMode>,
+
+  </React.StrictMode>  
+  
+  </Router>,
   document.getElementById('root')
 );
 
