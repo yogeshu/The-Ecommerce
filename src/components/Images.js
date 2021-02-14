fae0c21f1feb475b6e15be5939dfcf36f383abb4
yogeshu <yogeshbhavsar1994@gmail.com>
2021-02-14 15:20:01 +0530

    added hover state to show icon on iamge

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,8 +1,12 @@
 import React from "react"
 
 function Image({className,img}) {
+    const [hover,setHover] = React.useState(false)
     return (
-        <div className={`${className} image-container`}>
+        <div className={`${className} image-container`}
+         onMouseEnter={()=> setHover(true)}
+         onMouseLeave={()=> setHover(false)}
+        >
             <img src={img.url} className="image-grid"/>
         </div>
     )
