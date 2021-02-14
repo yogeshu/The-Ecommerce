e54ee363e408f6fe99f5349febfc54f129ac3050
yogeshu <yogeshbhavsar1994@gmail.com>
2021-02-14 15:22:13 +0530

    Icon added for favorite and card option on image

--- src/components/Images.js
+++ src/components/Images.js
@@ -2,12 +2,16 @@ import React from "react"
 
 function Image({className,img}) {
     const [hover,setHover] = React.useState(false)
+    const heartIcon = hover && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hover && <i className="ri-add-circle-line cart"></i>
     return (
         <div className={`${className} image-container`}
          onMouseEnter={()=> setHover(true)}
          onMouseLeave={()=> setHover(false)}
         >
             <img src={img.url} className="image-grid"/>
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
